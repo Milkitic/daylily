ecb4acfc7947852b7dfcafe01685d4611b44fa1e
milkitic <milkitic@126.com>
2018-07-23 14:15:35 +0800

    更新挖坑插件

--- Daylily.Common/Function/Application/Command/MyGraveyard.cs
+++ Daylily.Common/Function/Application/Command/MyGraveyard.cs
@@ -26,71 +26,51 @@ namespace Daylily.Common.Function.Application.Command
     [Command("挖坑")]
     public class MyGraveyard : CommandApp
     {
-        private CommonMessage _message;
-        private Thread _t;
-
         public override void Initialize(string[] args)
         {
 
         }
+
         public override CommonMessageResponse Message_Received(in CommonMessage messageObj)
         {
-            _message = messageObj;
-            _t = new Thread(Async);
-            _t.Start();
-            return null;
-        }
-
-        private void Async()
-        {
-            try
+            BllUserRole bllUserRole = new BllUserRole();
+            List<TblUserRole> userInfo = bllUserRole.GetUserRoleByQq(long.Parse(messageObj.UserId));
+            if (userInfo.Count == 0)
             {
-                BllUserRole bllUserRole = new BllUserRole();
-                List<TblUserRole> userInfo = bllUserRole.GetUserRoleByQq(long.Parse(_message.UserId));
-                if (userInfo.Count == 0)
-                {
-                    SendMessage(new CommonMessageResponse(LoliReply.IdNotBound, _message, true));
-                    return;
-                }
-
-                var id = userInfo[0].UserId.ToString();
-
-                List<Beatmap> totalList = new List<Beatmap>();
-                Beatmap[] tmpArray;
-                int page = 0;
-                const int count = 10;
-                do
-                {
-                    string json = WebRequestUtil.GetResponseString(
-                        WebRequestUtil.CreateGetHttpResponse(
-                            "https://osu.ppy.sh/users/" + id + "/beatmapsets/graveyard?offset=" + page + "&limit=" + count));
-                    Logger.Debug("GET JSON");
-
-                    tmpArray = JsonConvert.DeserializeObject<Beatmap[]>(json);
-                    totalList.AddRange(tmpArray);
-                    page += count;
-
-                    if (tmpArray.Length != count) break;
-                } while (tmpArray.Length != 0);
-
-                if (totalList.Count == 0)
-                {
-                    SendMessage(new CommonMessageResponse("惊了，你竟然会没坑！", _message, true));
-                    return;
-                }
-
-                Random rnd = new Random();
-                Beatmap beatmap = totalList[rnd.Next(totalList.Count)];
-                var cqMusic = new CustomMusic("https://osu.ppy.sh/s/" + beatmap.Id, $"https://b.ppy.sh/preview/{beatmap.Id}.mp3", beatmap.Title,
-                    $"{beatmap.Artist}\r\n({beatmap.FavouriteCount} fav)", $"https://b.ppy.sh/thumb/{beatmap.Id}l.jpg");
-
-                SendMessage(new CommonMessageResponse(cqMusic.ToString(), _message));
+                return new CommonMessageResponse(LoliReply.IdNotBound, messageObj, true);
             }
-            catch (Exception ex)
+
+            var id = userInfo[0].UserId.ToString();
+
+            List<Beatmap> totalList = new List<Beatmap>();
+            Beatmap[] tmpArray;
+            int page = 0;
+            const int count = 10;
+            do
             {
-                Logger.Exception(ex);
-            }
-        }
+                string json = WebRequestUtil.GetResponseString(
+                    WebRequestUtil.CreateGetHttpResponse(
+                        "https://osu.ppy.sh/users/" + id + "/beatmapsets/graveyard?offset=" + page + "&limit=" + count));
+                Logger.Debug("GET JSON");
 
+                tmpArray = JsonConvert.DeserializeObject<Beatmap[]>(json);
+                totalList.AddRange(tmpArray);
+                page += count;
+
+                if (tmpArray.Length != count) break;
+            } while (tmpArray.Length != 0);
+
+            if (totalList.Count == 0)
+            {
+                return new CommonMessageResponse("惊了，你竟然会没坑！", messageObj, true);
+            }
+
+            Random rnd = new Random();
+            Beatmap beatmap = totalList[rnd.Next(totalList.Count)];
+            var cqMusic = new CustomMusic("https://osu.ppy.sh/s/" + beatmap.Id, $"https://b.ppy.sh/preview/{beatmap.Id}.mp3", beatmap.Title,
+                $"{beatmap.Artist}\r\n({beatmap.FavouriteCount} fav)", $"https://b.ppy.sh/thumb/{beatmap.Id}l.jpg");
+
+            return new CommonMessageResponse(cqMusic.ToString(), messageObj);
+        }
     }
 }
