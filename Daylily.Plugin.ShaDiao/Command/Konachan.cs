44b8d4d8bea35de17e28f2358e81595084643462
bleatingsheep <bleatingsheep@live.cn>
2018-08-30 21:30:51 +0800

    增加 Konachan 热门图片功能。

---
+++ Daylily.Plugin.ShaDiao/Command/Konachan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Daylily.Bot.Attributes;
+using Daylily.Bot.Models;
+using Daylily.Bot.PluginBase;
+using Daylily.CoolQ;
+
+namespace Daylily.Plugin.ShaDiao.Command
+{
+    [Command("konachan")]
+    public class Konachan : CommandPlugin
+    {
+        private const string Website = "https://konachan.net";
+
+        public override void Initialize(string[] args)
+        {
+            return;
+        }
+
+        public override CommonMessageResponse Message_Received(CommonMessage messageObj)
+        {
+            var k = new Moebooru.Api(Website);
+            var result = k.PopularRecentAsync().Result;
+            if (!result.Any())
+                return null;
+            var post = result.First();
+            return new CommonMessageResponse(new FileImage(post.JpegUrl, true).ToString(), messageObj);
+        }
+    }
+}
