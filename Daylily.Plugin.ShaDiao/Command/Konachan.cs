ef2546e02104a240a05a6611d5d1dfa99f3d6728
bleatingsheep <bleatingsheep@live.cn>
2018-09-13 15:15:56 +0800

    合并到一个类。

--- Daylily.Plugin.ShaDiao/Command/Konachan.cs
+++ Daylily.Plugin.ShaDiao/Command/Konachan.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using Daylily.Bot.Attributes;
 using Daylily.Bot.Enum;
@@ -10,12 +12,24 @@ namespace Daylily.Plugin.ShaDiao.Command
 {
     [Name("konachan")]
     [Author("bleatingsheep")]
-    [Version(0, 0, 2, PluginVersion.Stable)]
+    [Version(0, 0, 23, PluginVersion.Stable)]
     [Help("设了")]
-    [Command("konachan")]
+    [Command("konachan", "yandere", "safebooru")]
     public class Konachan : CommandPlugin
     {
-        protected virtual string Website => "https://konachan.net";
+        private static IDictionary<string, string> WebsiteMap => new Dictionary<string, string>
+        {
+            { "konachan".ToUpperInvariant(), "https://konachan.net" },
+            { "yandere".ToUpperInvariant(), "https://yande.re" },
+        };
+
+        private static readonly IReadOnlyDictionary<string, string> Websites = new ReadOnlyDictionary<string, string>(WebsiteMap);
+
+        private string GetWebsite(CommonMessage messageObj)
+        {
+            var name = messageObj.Command.ToUpperInvariant();
+            return Websites.GetValueOrDefault(name);
+        }
 
         public override void Initialize(string[] args)
         {
@@ -24,12 +38,16 @@ namespace Daylily.Plugin.ShaDiao.Command
 
         public override CommonMessageResponse Message_Received(CommonMessage messageObj)
         {
-            var k = new Moebooru.Api(Website);
+            var domain = GetWebsite(messageObj);
+            if (string.IsNullOrEmpty(domain))
+            {
+                return new CommonMessageResponse("并不支持这个网站哦~~", messageObj);
+            }
+
+            var k = new Moebooru.Api(domain);
             var result = k.PopularRecentAsync().Result;
-            if (!result.Any())
-                return null;
-            var post = result.First();
-            return new CommonMessageResponse(new FileImage(post.JpegUrl, true).ToString(), messageObj);
+            var post = result?.FirstOrDefault();
+            return post == null ? null : new CommonMessageResponse(new FileImage(post.JpegUrl, true).ToString(), messageObj);
         }
     }
 }
