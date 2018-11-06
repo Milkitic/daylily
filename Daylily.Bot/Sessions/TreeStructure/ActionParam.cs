253bb88cadc01ccda5a744dd70b27cb1545709b5
milkitic <milkitic@126.com>
2018-11-06 18:10:03 +0800

    session功能更新

---
+++ Daylily.Bot/Sessions/TreeStructure/ActionParam.cs
@@ -0,0 +1,19 @@
+﻿namespace Daylily.Bot.Sessions.TreeStructure
+{
+    public struct ActionParam
+    {
+        public ActionParam(string targetName) : this()
+        {
+            TargetName = targetName;
+        }
+
+        public ActionParam(string targetName, object targetParam) : this()
+        {
+            TargetName = targetName;
+            TargetParam = targetParam;
+        }
+
+        public string TargetName { get; set; }
+        public object TargetParam { get; set; }
+    }
+}
---
+++ Daylily.Bot/Sessions/TreeStructure/NavigatableNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Daylily.Bot.Sessions.TreeStructure
+{
+    public class NavigatableNode : Node
+    {
+        public NavigatableNode(string name) : base(name)
+        {
+        }
+
+        public NavigatableNode(string name, Func<object, ActionParam> actionCallback) : base(name)
+        {
+            Action = actionCallback;
+        }
+
+        public Func<object, ActionParam> Action { get; set; }
+    }
+}
