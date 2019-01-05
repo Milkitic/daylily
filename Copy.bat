92973370675014740800bb23a958499486deb0f8
milkitic <milkitic@126.com>
2019-01-06 05:33:56 +0800

    change project name

--- Copy.bat
+++ Copy.bat
@@ -1,5 +1,5 @@
 copy Daylily.Plugin.Kernel\bin\Debug\netcoreapp2.2\Daylily.Plugin.Kernel.dll Daylily.AspNetCore\bin\Debug\netcoreapp2.2\Plugin\Daylily.Plugin.Kernel.dll
-copy Daylily.Plugin.ShaDiao\bin\Debug\netcoreapp2.2\Daylily.Plugin.ShaDiao.dll Daylily.AspNetCore\bin\Debug\netcoreapp2.2\Plugin\Daylily.Plugin.ShaDiao.dll
+copy Daylily.Plugin.Fun\bin\Debug\netcoreapp2.2\Daylily.Plugin.Fun.dll Daylily.AspNetCore\bin\Debug\netcoreapp2.2\Plugin\Daylily.Plugin.Fun.dll
 copy Daylily.Plugin.Basic\bin\Debug\netcoreapp2.2\Daylily.Plugin.Basic.dll Daylily.AspNetCore\bin\Debug\netcoreapp2.2\Plugin\Daylily.Plugin.Basic.dll
 copy Daylily.Plugin.Osu\bin\Debug\netcoreapp2.2\Daylily.Plugin.Osu.dll Daylily.AspNetCore\bin\Debug\netcoreapp2.2\Plugin\Daylily.Plugin.Osu.dll 
 copy Daylily.Plugin.Kernel\bin\Release\netcoreapp2.2\Daylily.Plugin.Kernel.dll Daylily.AspNetCore\bin\Release\PublishOutput\Plugin\Daylily.Plugin.Kernel.dll
