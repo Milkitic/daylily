996ff9bdd5fae97878bd6f37a72739614e1ee7d7
milkitic <milkitic@126.com>
2019-01-06 05:15:44 +0800

    change path

--- Copy.bat
+++ Copy.bat
@@ -1,6 +1,6 @@
 copy Daylily.Plugin.Kernel\bin\Debug\netcoreapp2.2\Daylily.Plugin.Kernel.dll Daylily.AspNetCore\bin\Debug\netcoreapp2.2\Plugin\Daylily.Plugin.Kernel.dll
 copy Daylily.Plugin.ShaDiao\bin\Debug\netcoreapp2.2\Daylily.Plugin.ShaDiao.dll Daylily.AspNetCore\bin\Debug\netcoreapp2.2\Plugin\Daylily.Plugin.ShaDiao.dll
-copy Daylily.Plugin.Core\bin\Debug\netcoreapp2.2\Daylily.Plugin.Basic.dll Daylily.AspNetCore\bin\Debug\netcoreapp2.2\Plugin\Daylily.Plugin.Basic.dll
+copy Daylily.Plugin.Basic\bin\Debug\netcoreapp2.2\Daylily.Plugin.Basic.dll Daylily.AspNetCore\bin\Debug\netcoreapp2.2\Plugin\Daylily.Plugin.Basic.dll
 copy Daylily.Plugin.Osu\bin\Debug\netcoreapp2.2\Daylily.Plugin.Osu.dll Daylily.AspNetCore\bin\Debug\netcoreapp2.2\Plugin\Daylily.Plugin.Osu.dll 
 copy Daylily.Plugin.Kernel\bin\Release\netcoreapp2.2\Daylily.Plugin.Kernel.dll Daylily.AspNetCore\bin\Release\PublishOutput\Plugin\Daylily.Plugin.Kernel.dll
 copy Daylily.Plugin.ShaDiao\bin\Release\netcoreapp2.2\Daylily.Plugin.ShaDiao.dll Daylily.AspNetCore\bin\Release\PublishOutput\Plugin\Daylily.Plugin.ShaDiao.dll
