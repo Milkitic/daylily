00936dde3628c3fb5857cbd10fae952ab9383f5a
Milkitic <Milkitic@126.com>
2022-03-07 21:21:57 +0800

    fix silly code

--- daylily/DefaultReply.cs
+++ daylily/DefaultReply.cs
@@ -2,6 +2,8 @@
 
 public static class DefaultReply
 {
+    private static readonly string[] FakeRootList = { "你没有顶级权限。" };
+
     public const string PrivateOnly = "此功能仅限私聊。";
     public const string GroupDiscussOnly = "此功能仅限群聊。";
 
@@ -11,14 +13,7 @@ public static class DefaultReply
     public const string ParamError = "命令后参数不正确，请使用 /help [命令] 查看说明。";
     public const string ParamMissing = "请填写参数，请使用 /help [命令] 查看说明。";
 
-    public static string FakeRoot
-    {
-        get
-        {
-            string[] exp = { "你没有顶级权限。" };
-            return exp[Random.Shared.Next(0, exp.Length)];
-        }
-    }
+    public static string FakeRoot => FakeRootList[Random.Shared.Next(0, FakeRootList.Length)];
 
     public const string FakeAdmin = "你非此群管理员，无法使用提权命令。";
 }
