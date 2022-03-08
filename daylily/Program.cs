5363d71da802d8feb0507aa423ea03b18f8b77ea
Milkitic <Milkitic@126.com>
2022-03-08 11:33:31 +0800

    use nlog instead

--- daylily/Program.cs
+++ daylily/Program.cs
@@ -1,30 +1,17 @@
 ﻿using Microsoft.Extensions.Logging;
 using MilkiBotFramework.Aspnetcore;
 using MilkiBotFramework.Platforms.GoCqHttp;
+using NLog.Extensions.Logging;
 
-return await new AspnetcoreBotBuilder(args, "http://0.0.0.0:23333")
-    //.UseGoCqHttp("ws://127.0.0.1:6700")
-    .UseGoCqHttp(GoCqConnection.ReverseWebSocket("/connector/reversews"))
-    //.UseGoCqHttp(GoCqConnection.WebSocket("ws://127.0.0.1:6700"))
+return await new AspnetcoreBotBuilder()
+    .UseGoCqHttp()
     .ConfigureLogger(k => k
-        .AddSimpleConsole(options =>
-        {
-            options.IncludeScopes = true;
-            //options.SingleLine = true;
-            options.TimestampFormat = "hh:mm:ss.ffzz ";
-        })
-        .AddFilter((ns, level) =>
-        {
-            if (ns.StartsWith("Microsoft") && level < LogLevel.Warning)
-                return false;
-            if (level < LogLevel.Information)
-                return false;
-            return true;
+#if DEBUG
+            .AddNLog("NLog.debug.config")
+#else
+            .AddNLog("NLog.config")
+#endif
 
-            //if (ns.StartsWith("Microsoft") && level < LogLevel.Information)
-            //    return false;
-            //return true;
-        })
     )
     .Build()
     .RunAsync();
