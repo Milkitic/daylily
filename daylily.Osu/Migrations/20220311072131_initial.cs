48869604e034905aae8bd42021c05c289e25ba19
Milkitic <Milkitic@126.com>
2022-03-11 15:22:08 +0800

    add initial migration

---
+++ daylily.Osu/Migrations/20220311072131_initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace daylily.Osu.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tokens",
+                columns: table => new
+                {
+                    qq = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TokenType = table.Column<string>(type: "TEXT", nullable: false),
+                    ExpiresIn = table.Column<long>(type: "INTEGER", nullable: false),
+                    AccessToken = table.Column<string>(type: "TEXT", nullable: false),
+                    CreateTime = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    RefreshToken = table.Column<string>(type: "TEXT", nullable: false),
+                    IsPublic = table.Column<bool>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<long>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tokens", x => x.qq);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tokens");
+        }
+    }
+}
