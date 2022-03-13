30c1d690acc6185e5f8854084d2a0a4a24757016
Milkitic <Milkitic@126.com>
2022-03-13 20:29:46 +0800

    add apis

--- daylily/Plugins/Osu/ApiService.cs
+++ daylily/Plugins/Osu/ApiService.cs
@@ -1,6 +1,6 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using Coosu.Api.V2;
+﻿using Coosu.Api.V2;
 using Coosu.Api.V2.ResponseModels;
+using daylily.Data;
 using Microsoft.Extensions.Logging;
 using MilkiBotFramework.Messaging;
 using MilkiBotFramework.Plugining;
@@ -13,8 +13,28 @@ namespace daylily.Plugins.Osu;
 [PluginIdentifier("a1332794-448f-4378-a8b4-209428204af1")]
 public class ApiService : ServicePlugin
 {
+    public class ApiResult<T>
+    {
+        public ApiResult(string error)
+        {
+            Error = error;
+        }
+
+        public ApiResult(T? result)
+        {
+            Result = result;
+            Success = true;
+        }
+
+        public bool Success { get; set; }
+        public T? Result { get; set; }
+        public string? Error { get; set; }
+    }
+
     private readonly BotTaskScheduler _taskScheduler;
     private readonly OsuConfig _config;
+
+    internal string UnbindMessage => "你还没有绑定账号，请私聊我 \"/set-osuid\" 完成绑定。直接复制引号中的内容即可。";
 
     public ApiService(IConfiguration<OsuConfig> configuration, BotTaskScheduler taskScheduler)
     {
@@ -24,34 +44,52 @@ public class ApiService : ServicePlugin
 
     public UserToken? PublicToken { get; private set; }
 
-    internal bool TryAccessApi<T>(TokenBase token, Func<OsuClientV2, T> func,
-        [NotNullWhen(true)] out T? result,
-        [NotNullWhen(false)] out IResponse? accessErrorMessage)
+    internal async Task<ApiResult<T>> TryAccessPublicApi<T>(Func<OsuClientV2, Task<T>> func)
     {
         try
         {
-            result = func.Invoke(new OsuClientV2(token))!;
-            accessErrorMessage = null;
-            return true;
+            if (PublicToken == null)
+                return new ApiResult<T>("osu PublicToken 授权出错");
+            return new ApiResult<T>(await func.Invoke(new OsuClientV2(PublicToken)));
         }
         catch (HttpRequestException ex)
         {
             if (ex.Message.Contains("401"))
             {
-                accessErrorMessage = Reply("osu授权出错，请重新绑定osu ID");
-            }
-            else if (ex.Message.Contains("404"))
-            {
-                accessErrorMessage = Reply("无结果，请检查参数");
-            }
-            else
-            {
-                accessErrorMessage = Reply("osu接口访问出错：" + ex.Message);
+                return new ApiResult<T>("osu授权出错，请重新绑定osu ID");
             }
 
-            result = default;
-            return false;
+            if (ex.Message.Contains("404"))
+            {
+                return new ApiResult<T>("无结果，请检查参数");
+            }
+
+            return new ApiResult<T>("osu!api访问出错：" + ex.Message);
         }
+    }
+
+    internal async Task<(bool, TokenBase? token, IResponse? unbindMessage)> TryGetToken(
+        MessageContext messageContext, OsuDbContext dbContext)
+    {
+        var osuToken = await dbContext.Tokens.FindAsync(long.Parse(messageContext.MessageUserIdentity.UserId));
+        if (osuToken == null)
+        {
+            return (false, null, Reply(UnbindMessage));
+        }
+
+        return (true, ConvertToken(osuToken), null);
+    }
+
+    internal async Task<(bool, long? osuId, IResponse? message)> TryGetUserId(
+        MessageContext messageContext, OsuDbContext dbContext)
+    {
+        var osuId = dbContext.GetUserIdByQQ(Convert.ToInt64(messageContext.MessageUserIdentity.UserId)).Result;
+        if (osuId == null)
+        {
+            return (false, null, Reply(UnbindMessage));
+        }
+
+        return (false, osuId, null);
     }
 
     protected override async Task OnInitialized()
@@ -69,5 +107,24 @@ public class ApiService : ServicePlugin
         var result = authClientPub.GetPublicToken(_config.ClientId, _config.ClientSecret).Result;
         this.PublicToken = result;
         context.Logger.LogInformation($"已刷新Osu!PublicToken，将在{DateTime.Now.AddSeconds(result.ExpiresIn)}过期");
+    }
+    private static TokenBase ConvertToken(OsuToken osuToken)
+    {
+        if (osuToken.IsPublic)
+            return new PublicToken
+            {
+                AccessToken = osuToken.AccessToken,
+                CreateTime = osuToken.CreateTime,
+                ExpiresIn = osuToken.ExpiresIn,
+                TokenType = osuToken.TokenType
+            };
+        return new UserToken
+        {
+            AccessToken = osuToken.AccessToken,
+            CreateTime = osuToken.CreateTime,
+            ExpiresIn = osuToken.ExpiresIn,
+            TokenType = osuToken.TokenType,
+            RefreshToken = osuToken.RefreshToken
+        };
     }
 }
---
+++ daylily/Plugins/Osu/MePlugin.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+using System.Text;
+using Coosu.Api.V2;
+using daylily.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MilkiBotFramework.Messaging;
+using MilkiBotFramework.Messaging.RichMessages;
+using MilkiBotFramework.Plugining;
+using MilkiBotFramework.Plugining.Attributes;
+
+namespace daylily.Plugins.Osu;
+
+[PluginIdentifier("1d3dce2d-f52d-42ab-97a4-fcf31e11dfbc", "个人名片", Authors = "milkiyf")]
+[Description("个人名片查询")]
+public class MePlugin : BasicPlugin
+{
+    private readonly ILogger<MePlugin> _logger;
+    private readonly ApiService _apiService;
+    private readonly OsuDbContext _dbContext;
+
+    public MePlugin(ILogger<MePlugin> logger, ApiService apiService, OsuDbContext dbContext)
+    {
+        _logger = logger;
+        _apiService = apiService;
+        _dbContext = dbContext;
+    }
+
+    [CommandHandler("me.osu")]
+    public async Task<IResponse> MeOsu(MessageContext messageContext)
+    {
+        var str = _dbContext.Database.GetDbConnection().ConnectionString;
+        var osuId = await _dbContext.GetUserIdByQQ(Convert.ToInt64(messageContext.MessageUserIdentity.UserId));
+        if (osuId == null) return Reply(_apiService.UnbindMessage);
+        var result = await _apiService.TryAccessPublicApi(async client =>
+            await client.User.GetUser(osuId.ToString(), GameMode.Osu));
+        if (!result.Success)
+        {
+            return Reply(result.Error!);
+        }
+
+        var user = result.Result;
+
+        var sb = new StringBuilder();
+        sb.AppendLine(user.Username + "  " +
+                      (string.IsNullOrWhiteSpace(user.Location) ? "" : (user.Location + ", ")) +
+                      user.Country?.Name);
+        if (!string.IsNullOrWhiteSpace(user.Discord)) sb.AppendLine("Discord: " + user.Discord);
+        if (!string.IsNullOrWhiteSpace(user.Title)) sb.AppendLine("职位: " + user.Title);
+        sb.AppendLine("好友关注: " + user.FollowerCount);
+        sb.AppendLine("Rank图: " + user.RankedAndApprovedBeatmapsetCount);
+        sb.AppendLine("Pending图: " + user.UnrankedBeatmapsetCount);
+        sb.AppendLine("坟图: " + user.GraveyardBeatmapsetCount);
+        if (user.Badges is { Length: > 0 })
+        {
+            sb.AppendLine("狗牌: ");
+            var richMessage = new RichMessage(new Text(sb.ToString()));
+            foreach (var badge in user.Badges)
+            {
+                _logger.LogDebug(badge.ImageUrl);
+                richMessage.RichMessages.Add(new LinkImage(badge.ImageUrl));
+            }
+
+            return Reply(richMessage);
+        }
+        else
+        {
+            return Reply(sb.ToString().Trim('\n', '\r'));
+        }
+
+    }
+}
