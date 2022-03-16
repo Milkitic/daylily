db19a254e07c1273fd47ce97a2e5c22afc05c7a2
Milkitic <Milkitic@126.com>
2022-03-16 15:57:25 +0800

    change dbcontext

--- daylily/Plugins/Osu/Data/OsuDbContext.cs
+++ daylily/Plugins/Osu/Data/OsuDbContext.cs
@@ -1,62 +1,77 @@
 ﻿using Coosu.Api.V2.ResponseModels;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using MilkiBotFramework.Plugining.Database;
 
-namespace daylily.Plugins.Osu.Data
+namespace daylily.Plugins.Osu.Data;
+
+public class OsuDbContext : PluginDbContext
 {
-    public class OsuDbContext : PluginDbContext
+    public DbSet<OsuToken> Tokens { get; set; }
+    public DbSet<OsuUserInfo> OsuUserInfos { get; set; }
+    public DbSet<BeatmapScan> BeatmapScans { get; set; }
+    public DbSet<BeatmapStat> BeatmapStats { get; set; }
+    public DbSet<BeatmapSubscribe> BeatmapSubscribes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public DbSet<OsuToken> Tokens { get; set; }
-        public DbSet<OsuUserInfo> OsuUserInfos { get; set; }
-        public DbSet<BeatmapScan> BeatmapScans { get; set; }
-        public DbSet<BeatmapStat> BeatmapStats { get; set; }
-        public DbSet<BeatmapSubscribe> BeatmapSubscribes { get; set; }
+        modelBuilder
+            .Entity<OsuUserInfo>()
+            .Property(k => k.RequestStatus)
+            .HasConversion(
+                v => (v == null || v.Length == 0) ? default : string.Join(',', v),
+                v => v == default
+                    ? Array.Empty<RequestStatus>()
+                    : v.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(Enum.Parse<RequestStatus>)
+                        .ToArray(),
+                new ValueComparer<RequestStatus[]?>(true)
+            );
+    }
 
-        public async Task<long?> GetUserIdBySourceId(string qq)
+    public async Task<long?> GetUserIdBySourceId(string qq)
+    {
+        var result = (await Tokens.FindAsync(qq))?.UserId;
+        return result;
+    }
+
+    public async Task AddOrUpdateToken(string sourceId, long userId, TokenBase token)
+    {
+        var osuToken = new OsuToken
         {
-            var result = (await Tokens.FindAsync(qq))?.UserId;
-            return result;
+            AccessToken = token.AccessToken,
+            TokenType = token.TokenType,
+            CreateTime = token.CreateTime,
+            ExpiresIn = token.ExpiresIn,
+            SourceId = sourceId,
+            UserId = userId
+        };
+
+        if (token is UserToken userToken)
+        {
+            osuToken.RefreshToken = userToken.RefreshToken;
+        }
+        else
+        {
+            osuToken.IsPublic = true;
         }
 
-        public async Task AddOrUpdateToken(string sourceId, long userId, TokenBase token)
+        var exist = await Tokens.FindAsync(sourceId);
+        if (exist == null)
         {
-            var osuToken = new OsuToken
-            {
-                AccessToken = token.AccessToken,
-                TokenType = token.TokenType,
-                CreateTime = token.CreateTime,
-                ExpiresIn = token.ExpiresIn,
-                SourceId = sourceId,
-                UserId = userId
-            };
-
-            if (token is UserToken userToken)
-            {
-                osuToken.RefreshToken = userToken.RefreshToken;
-            }
-            else
-            {
-                osuToken.IsPublic = true;
-            }
-
-            var exist = await Tokens.FindAsync(sourceId);
-            if (exist == null)
-            {
-                Tokens.Add(osuToken);
-            }
-            else
-            {
-                exist.TokenType = token.TokenType;
-                exist.AccessToken = token.AccessToken;
-                exist.CreateTime = token.CreateTime;
-                exist.ExpiresIn = token.ExpiresIn;
-                exist.IsPublic = osuToken.IsPublic;
-                exist.RefreshToken = exist.RefreshToken;
-                exist.UserId = userId;
-            }
-
-            await SaveChangesAsync();
+            Tokens.Add(osuToken);
         }
+        else
+        {
+            exist.TokenType = token.TokenType;
+            exist.AccessToken = token.AccessToken;
+            exist.CreateTime = token.CreateTime;
+            exist.ExpiresIn = token.ExpiresIn;
+            exist.IsPublic = osuToken.IsPublic;
+            exist.RefreshToken = exist.RefreshToken;
+            exist.UserId = userId;
+        }
+
+        await SaveChangesAsync();
     }
 }
